be640eb1feb5c027d857b7f1c176a780fe6823ee
phuongez <duyphuong.hvnh@gmail.com>
2025-03-11 15:12:53 +0700

    First commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const port = process.env.PORT || 8000
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"))
+});
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "about.html"))
+});
+app.get("/contact-me", (req, res) => {
+    res.sendFile(path.join(__dirname, "contact-me.html"))
+});
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "404.html"))
+});
+
+app.listen(PORT, () => {
+  console.log(`My first Express app - listening on port ${PORT}!`);
+});
